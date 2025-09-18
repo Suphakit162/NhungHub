05352f986e7ce3fdf16613de77ff07ba4434dd48
Naruedon-mu <marknarudon@gmail.com>
2025-09-18 16:23:46 +0700

    .

--- nhunghub.sql
+++ nhunghub.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 17.5
 -- Dumped by pg_dump version 17.5
 
--- Started on 2025-09-14 13:46:36
+-- Started on 2025-09-18 16:23:26
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -20,7 +20,19 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- TOC entry 4901 (class 0 OID 24901)
+-- TOC entry 4911 (class 0 OID 24910)
+-- Dependencies: 222
+-- Data for Name: Admins; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public."Admins" (id, name, password, "createdAt", "updatedAt") FROM stdin;
+1	nhunghubadmin	loadingmovie66	2025-09-18 15:56:23.333+07	2025-09-18 15:56:23.333+07
+2	nhunghubadmin	nhunghub6789	2025-09-18 15:59:42.753+07	2025-09-18 15:59:42.753+07
+\.
+
+
+--
+-- TOC entry 4909 (class 0 OID 24901)
 -- Dependencies: 220
 -- Data for Name: Movies; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -36,11 +48,12 @@ COPY public."Movies" (id, name, image, rating, description, genre, review, "crea
 8	Parasite	parasite.jpg	5	A poor family schemes to infiltrate a wealthy household.	{Thriller,Drama}	{"Brilliant plot.","Clever social commentary."}	2025-09-14 11:15:51.47+07	2025-09-14 11:15:51.47+07
 9	The Lion King	lionking.jpg	5	A young lion prince flees his kingdom only to learn the true meaning of responsibility.	{Animation,Adventure,Family}	{"Timeless classic.","Loved the songs."}	2025-09-14 11:15:51.47+07	2025-09-14 11:15:51.47+07
 10	Spider-Man: No Way Home	spiderman.jpg	5	Spider-Man faces multiverse villains while trying to protect his identity.	{Action,Adventure,Sci-Fi}	{"Fan service done right.","Exciting and emotional."}	2025-09-14 11:15:51.47+07	2025-09-14 11:15:51.47+07
+11	Inception	inception.jpg	5	A thief steals corporate secrets through dream-sharing technology.	{Sci-Fi,Action}	{"Amazing movie!","Mind-blowing visuals!"}	2025-09-18 15:23:37.007+07	2025-09-18 15:23:37.007+07
 \.
 
 
 --
--- TOC entry 4899 (class 0 OID 24883)
+-- TOC entry 4907 (class 0 OID 24883)
 -- Dependencies: 218
 -- Data for Name: Users; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -60,16 +73,25 @@ COPY public."Users" (id, username, email, password, "createdAt", "updatedAt") FR
 
 
 --
--- TOC entry 4909 (class 0 OID 0)
+-- TOC entry 4920 (class 0 OID 0)
+-- Dependencies: 221
+-- Name: Admins_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public."Admins_id_seq"', 2, true);
+
+
+--
+-- TOC entry 4921 (class 0 OID 0)
 -- Dependencies: 219
 -- Name: Movies_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public."Movies_id_seq"', 10, true);
+SELECT pg_catalog.setval('public."Movies_id_seq"', 11, true);
 
 
 --
--- TOC entry 4910 (class 0 OID 0)
+-- TOC entry 4922 (class 0 OID 0)
 -- Dependencies: 217
 -- Name: Users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
@@ -77,7 +99,7 @@ SELECT pg_catalog.setval('public."Movies_id_seq"', 10, true);
 SELECT pg_catalog.setval('public."Users_id_seq"', 10, true);
 
 
--- Completed on 2025-09-14 13:46:36
+-- Completed on 2025-09-18 16:23:26
 
 --
 -- PostgreSQL database dump complete
